560c75f8e2daf6debf2db3c25058c2a5270e1ba5
Martin Lyu <info@soln.ai>
2026-01-03 18:09:10 -0500

    auto-claude: 4.1 - Update widgets index.ts to export all widgets

--- analytics-dashboard/src/components/widgets/index.ts
+++ analytics-dashboard/src/components/widgets/index.ts
@@ -4,8 +4,6 @@
  * Exports all widget components for the analytics dashboard
  */
 
+export { AnomalyWidget } from "./AnomalyWidget";
 export { InsightWidget } from "./InsightWidget";
-
-// Additional widgets will be added as they are implemented:
-// export { AnomalyWidget } from "./AnomalyWidget";
-// export { PredictionWidget } from "./PredictionWidget";
+export { PredictionWidget } from "./PredictionWidget";
